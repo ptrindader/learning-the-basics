5ec19dec7106584574fdc8237999ef6a3e0b0651
ptrindader <pedro.treis@yahoo.com.br>
2022-12-08 18:21:49 -0300

    testingVSC to GH

---
+++ Lista Secao3/q1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Insira um número inteiro: ");
+    scanf("%d", &n);
+    printf("O número escolhido foi %d", n);
+    return 0;
+}
---
+++ Lista Secao3/q2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float x;
+    printf("Insira um número real (use o ponto como vírgula): ");
+    scanf("%f", &x);
+    printf("Seu número é %f", x);
+    return 0;
+}
