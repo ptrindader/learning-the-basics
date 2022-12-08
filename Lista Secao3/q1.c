071340bd6d29d749b9fc7071160d4df4837aa09b
ptrindader <pedro.treis@yahoo.com.br>
2022-12-08 18:33:34 -0300

    first commit

--- Lista Secao3/q1.c
+++ Lista Secao3/q1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
+    
     int n;
     printf("Insira um número inteiro: ");
     scanf("%d", &n);
     printf("O número escolhido foi %d", n);
+    
     return 0;
 }
