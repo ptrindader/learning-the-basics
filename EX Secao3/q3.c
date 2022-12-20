c2fda7fdf8d96f746e2f62d038565edd431aeea5
ptrindader <pedro.treis@yahoo.com.br>
2022-12-20 16:49:19 -0300

    q3 e executável da q2

---
+++ EX Secao3/q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x, y, z, soma;
+    printf("Digite 3 números: ");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    soma = x + y + z;
+    printf("A soma dos números é %d.", soma);
+    return 0;
+}
