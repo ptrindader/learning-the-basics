992f4f9bc93a0dea0df85fb8712649dbc36da064
ptrindader <pedro.treis@yahoo.com.br>
2022-12-21 01:42:09 -0300

    commit

--- C Language/exercicios-Secao3/q10.c
+++ C Language/exercicios-Secao3/q10.c
@@ -5,6 +5,6 @@ int main(){
     printf("Insira uma velocidade em km/h: ");
     scanf("%f", &kmh);
     ms = kmh/3.6;
-    printf("A velocidade de %0.2f km/h equivale a %0.2f m/s.", kmh, ms);
+    printf("A velocidade de %.2f km/h equivale a %.2f m/s.", kmh, ms);
     return 0;
 }
