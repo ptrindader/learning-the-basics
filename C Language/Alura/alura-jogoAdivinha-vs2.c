ff44c5261f2a3ac4c9c77e74d3c50425858fb2cc
ptrindader <pedro.treis@yahoo.com.br>
2023-01-19 00:40:33 -0300

    define and renaming variables

--- C Language/Alura/alura-jogoAdivinha-vs2.c
+++ C Language/Alura/alura-jogoAdivinha-vs2.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main() {
     // Imprime cabecalho do jogo
-    printf("------------------------------------------\n");
+    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
-    printf("------------------------------------------\n\n");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
 
-    int numeroSecreto = 42;
+    int numero_secreto = 42;
     int chute;
+    
+    // A variável numero_de_tentativas abaixo foi retirada para a implementação da constante de mesmo nome (em Caps) pela diretiva "define".
+    // int numero_de_tentativas = 3; 
+
 
     // o "for" executa o bloco de código até que a condição seja atendida.
 
     // Abaixo, foi declarada a variável "i", em seguida foi declarada a condição e, por fim, o incremento.
     // O incremento é o que o "for" faz após executar o bloco e antes de executá-lo novamente, se for o caso.
 
-    for(int i = 1; i <= 3; i++) {
+    for(int i = 1; i <=  NUMERO_DE_TENTATIVAS; i++) {
 
-        printf(">> TENTATIVA %d de 3\n", i);
+        printf(">> TENTATIVA %d de %d\n", i, NUMERO_DE_TENTATIVAS);
 
         printf("Qual é o seu chute? \n");
         scanf("%d", &chute);
         printf("\nSeu chute foi %d.\n", chute);
         
-        int acertou = (chute == numeroSecreto);
+        int acertou = (chute == numero_secreto);
 
 
         // OBS.: Igual-duplo é um igual para comparação
         
         if(acertou) { 
         printf("\nPARABÉNS! Você acertou!");
-        i = 3;  
+
+        // Aqui, pode tanto ser usado o "break" quanto o "i = 3". Ambos encerram o loop do "for".
+        // i = 3;  
+
+        break;
         }
         
         else {
 
-            if(chute > numeroSecreto) {
+            if(chute > numero_secreto) {
                 printf("Você errou! Seu chute foi MAIOR que o número secreto...\n\nTente novamente!\n\n--------------------- \n\n");
             }
 
