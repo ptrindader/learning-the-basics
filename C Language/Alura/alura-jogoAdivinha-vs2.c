42d3deca172cb3bbed120a882bc57f31b89dc7d0
ptrindader <pedro.treis@yahoo.com.br>
2023-01-26 21:46:28 -0300

    fix

--- C Language/Alura/alura-jogoAdivinha-vs2.c
+++ C Language/Alura/alura-jogoAdivinha-vs2.c
@@ -20,40 +20,48 @@ int main() {
     // Abaixo, foi declarada a variável "i", em seguida foi declarada a condição e, por fim, o incremento.
     // O incremento é o que o "for" faz após executar o bloco e antes de executá-lo novamente, se for o caso.
 
-    for(int i = 1; i <=  NUMERO_DE_TENTATIVAS; i++) {
 
+    // for(int i = 1; i <=  NUMERO_DE_TENTATIVAS; i++) {
+
+    while(ganhou == 0) {
+        
         printf(">> TENTATIVA %d de %d\n", i, NUMERO_DE_TENTATIVAS);
 
         printf("Qual é o seu chute? \n");
         scanf("%d", &chute);
         printf("\nSeu chute foi %d.\n", chute);
+
+        if (chute < 0) {
+            printf ("Você não pode chutar números negativos!\n\n");
+            i--;
+            continue; //"Reinicia" o loop sem ler o resto do bloco.
+        }
         
         int acertou = (chute == numero_secreto);
-
+        int maior = chute > numero_secreto;
+        int menor = chute < numero_secreto; //(variável não foi necessária) 
 
         // OBS.: Igual-duplo é um igual para comparação
         
         if(acertou) { 
-        printf("\nPARABÉNS! Você acertou!");
+            printf("\nPARABÉNS! Você acertou!");
 
-        // Aqui, pode tanto ser usado o "break" quanto o "i = 3". Ambos encerram o loop do "for".
-        // i = 3;  
+            // Aqui, pode tanto ser usado o "break" quanto o "i = 3". Ambos encerram o loop do "for".
+            // i = 3;  
 
-        break;
+            break;
         }
         
-        else {
-
-            if(chute > numero_secreto) {
-                printf("Você errou! Seu chute foi MAIOR que o número secreto...\n\nTente novamente!\n\n--------------------- \n\n");
-            }
-
-            else  {
-                printf("Você errou! Seu chute foi MENOR que o número secreto...\n\nTente novamente!\n\n--------------------- \n\n");
-            }
-        
+        else if(maior) {
+            printf("Você errou! Seu chute foi MAIOR que o número secreto...\n\nTente novamente!\n\n--------------------- \n\n");
         }
+
+        else  {
+            printf("Você errou! Seu chute foi MENOR que o número secreto...\n\nTente novamente!\n\n--------------------- \n\n");
+        }
+        
     }
+        
+    printf("\n\nFim de jogo!\n\n");
 
-    printf("\n\nFim de jogo!\n\n");   
 }
