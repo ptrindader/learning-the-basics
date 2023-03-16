fcc09abc7da96bf295981dd6b3eaab7f7ba95fd4
ptrindader <pedro.treis@yahoo.com.br>
2023-03-16 16:51:32 -0300

    commits variadas ufmg e organizacao

--- C Language/Alura/um-a-cem-while.c
+++ C Language/Alura/um-a-cem-while.c
@@ -2,11 +2,9 @@
 
 //Faça o mesmo programa do exercício anterior, só que dessa vez usando um 'while'.
 
-int main(){
-    
+int main(){ 
     int i = 1;
     while(i<=100){
-
         printf("%d\n", i);
         i++;
     }
