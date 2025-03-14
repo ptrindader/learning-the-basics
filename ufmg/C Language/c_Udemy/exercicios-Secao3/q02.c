fff2427e4335ea2c379bc7e8829bb8adf6e1f8dc
ptrindader <pedro.treis@yahoo.com.br>
2025-03-14 13:45:41 -0300

    Exclusão de Pastas para Correção

--- ufmg/C Language/c_Udemy/exercicios-Secao3/q02.c
+++
@@ -1,9 +0,0 @@
-//Ler e imprimir um Real
-#include <stdio.h>
-int main(){
-    float x;
-    printf("Insira um número real (use o ponto como vírgula): ");
-    scanf("%f", &x);
-    printf("Seu número é %f", x);
-    return 0;
-}
