bcd309e905b981f36ae50723ba5e8363d8cef777
ptrindader <pedro.treis@yahoo.com.br>
2025-04-03 17:32:28 -0300

    Desafio02, Aula 03

---
+++ estudos__frontEnd/JS__/desafios/aula03/d2__fatorial.js
@@ -0,0 +1,19 @@
+function calcularFatorial(numero) {
+    if (numero === 0 || numero === 1) {
+        return 1;
+    };
+
+    let fatorial = 1;
+
+    for (let i = 2; i <= numero; i++) {
+        fatorial *= i;
+    };
+
+    return fatorial;
+}
+
+//Usando:
+
+let numero = 5;
+let resultado = calcularFatorial(numero);
+console.log(`O fatorial de ${numero} é igual a ${resultado}`);
