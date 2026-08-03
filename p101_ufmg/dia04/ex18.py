ce95f62550a2cb86afeebf81e9e0babea8c0178f
ptrindader <pedro.treis@yahoo.com.br>
2026-08-03 14:15:24 -0300

    commit

--- p101_ufmg/dia04/ex18.py
+++ p101_ufmg/dia04/ex18.py
@@ -1,28 +1,22 @@
-def busca_binaria(lista, valor, inicio, fim):
-    # Lista ordenada
-    # Pesquisa um valor
-    # Retorna o índica de valor (ou -1 se nao encontrar).
+def busca_binaria(lista, valor, inicio, fim, cont=0):
+    cont += 1
+    if inicio > fim:
+        return -1
 
-    # Olhar o elemento do meio
-    # Se for maior que o valor, ir para a primeira parte da lista.
-    # Se for menor que o valor, ir para a segunda parte da lista.
-    lista = lista.sort
     indice_meio = (inicio + fim) // 2
     meio = lista[indice_meio]
+    
     if meio == valor:
-        return indice_meio
-    elif valor not in lista:
-        return -1
+        return indice_meio, cont
+    
     elif meio < valor:
-        inicio = indice_meio + 1
-        return busca_binaria(lista, valor, inicio, fim)
+        return busca_binaria(lista, valor, indice_meio + 1, fim, cont)
+    
     elif meio > valor:
-        fim = indice_meio - 1
-        return busca_binaria(lista, valor, inicio, fim)
+        return busca_binaria(lista, valor, inicio, indice_meio - 1, cont)
 
-lista_nums = [int(i) for i in range(100)]
+
+lista_nums = [int(i) for i in range(100)] 
 print(lista_nums)
 
-resultado = busca_binaria(lista_nums, 38, 0, "")
-
-print(resultado)
+resultado_bin, cont_bin = busca_binaria(lista_nums, 73, 0, )
